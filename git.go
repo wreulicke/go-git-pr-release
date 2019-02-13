124551465f8230ef3e17a28667dd391c545d9bc1
wreulicke <saito.masaya@classmethod.jp>
2019-02-14 04:58:41 +0900

    implement merge base and pull request

--- git.go
+++ git.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"errors"
 	"strings"
 
 	"gopkg.in/src-d/go-git.v4"
 	"gopkg.in/src-d/go-git.v4/plumbing"
+	"gopkg.in/src-d/go-git.v4/plumbing/object"
 )
 
 type PullRequestReference struct {
@@ -31,8 +33,54 @@ func FindPullRequestReference(r *git.Repository) ([]*plumbing.Reference, error) 
 	return result, nil
 }
 
-func FilterMergedPullRequest(r *git.Repository, refs []*plumbing.Reference) ([]*plumbing.Reference, error) {
-	c, _ := r.CommitObject(refs[0].Hash())
-	c.Parents().
-	return nil, nil
+// FindMergeBase provides like git merge-base
+func FindMergeBase(r *git.Repository, c1 *object.Commit, c2 *object.Commit) (*object.Commit, error) {
+	iter1 := object.NewCommitIterCTime(c1, nil, nil)
+	iter2 := object.NewCommitIterCTime(c2, nil, nil)
+	history := map[plumbing.Hash]bool{}
+
+	for {
+		var hasErrorOne bool
+		if c, err := iter1.Next(); err == nil {
+			if _, ok := history[c.Hash]; ok {
+				return c, nil
+			}
+			history[c.Hash] = true
+		} else {
+			hasErrorOne = true
+		}
+		if c, err := iter2.Next(); err == nil {
+			if _, ok := history[c.Hash]; ok {
+				return c, nil
+			}
+			history[c.Hash] = true
+		} else {
+			if hasErrorOne {
+				return nil, errors.New("merge-base is not found")
+			}
+		}
+	}
+}
+
+// FilterMergedPullRequest returns merged reference from refs.
+func FilterMergedPullRequest(r *git.Repository, productionRef *plumbing.Reference, refs []*plumbing.Reference) (results []*plumbing.Reference, err error) {
+	productionHead, err := r.CommitObject(productionRef.Hash())
+	if err != nil {
+		return nil, err
+	}
+
+	for _, ref := range refs {
+		c, err := r.CommitObject(ref.Hash())
+		if err != nil {
+			return nil, err
+		}
+		base, err := FindMergeBase(r, productionHead, c)
+		if err != nil {
+			return nil, err
+		}
+		if base.Hash == c.Hash {
+			results = append(results, ref)
+		}
+	}
+	return results, nil
 }
--- git_test.go
+++ git_test.go
@@ -4,10 +4,11 @@ import (
 	"testing"
 
 	"gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing"
 )
 
 func TestFindPullRequestReference(t *testing.T) {
-	// like ls-remote
+	// like git ls-remote origin refs/pull/*/head
 	r, err := git.PlainOpen(".")
 	if err != nil {
 		t.Error(err)
@@ -27,5 +28,45 @@ func TestFindPullRequestReference(t *testing.T) {
 	}
 	if !contains {
 		t.Error("Pull request reference is not found")
+	}
+}
+
+func TestFilterMergedPullRequest(t *testing.T) {
+	r, err := git.PlainOpen(".")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	refs, err := FindPullRequestReference(r)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	hash, err := r.ResolveRevision(plumbing.Revision("3704d81253329f6abff59a3ed6a542030ff1cabc"))
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	base := plumbing.NewHashReference("base", *hash)
+	results, err := FilterMergedPullRequest(r, base, refs)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	t.Log(results)
+	var contains bool
+	for _, v := range results {
+		if v.Name() == "refs/pull/1/head" {
+			contains = true
+		} else if v.Name() == "refs/pull/2/head" {
+			t.Error("refs/pull/2/head should not be contained")
+		}
+	}
+	if !contains {
+		t.Logf("Results: %v", results)
+		t.Error("refs/pull/1/head is not found")
 	}
 }
